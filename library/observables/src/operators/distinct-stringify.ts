b94e28f98904ac88b1f96b50f3d9a8d49bc67592
Nick Foscarini <nickf@reactgular.com>
2019-08-14 09:56:18 -0400

    updated

--- library/observables/src/operators/distinct-stringify.ts
+++ library/observables/src/operators/distinct-stringify.ts
@@ -1,7 +1,7 @@
-import {Observable, OperatorFunction} from 'rxjs';
+import {MonoTypeOperatorFunction, Observable} from 'rxjs';
 import {distinctUntilChanged} from 'rxjs/operators';
 
-export function distinctStringify<T>(): OperatorFunction<T, T> {
+export function distinctStringify<T>(): MonoTypeOperatorFunction<T> {
     return function (source: Observable<T>): Observable<T> {
         return source.pipe(distinctUntilChanged((a, b) => JSON.stringify(a) === JSON.stringify(b)));
     };
--- library/observables/src/utils/to-observable.ts
+++ library/observables/src/utils/to-observable.ts
@@ -3,6 +3,6 @@ import {isObservable, Observable, of} from 'rxjs';
 /**
  * Converts the parameter to an observable, or returns the value if already an observable.
  */
-export function toObservable<TType>(value: TType | Observable<TType>): Observable<TType> {
+export function toObservable<T>(value: T | Observable<T>): Observable<T> {
     return isObservable(value) ? value : of(value);
 }
