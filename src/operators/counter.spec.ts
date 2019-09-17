3cac8d569554141a874a521cd9c882a11dac5f47
codemile <nick@reactgular.com>
2019-09-17 09:07:58 -0400

    adds marbles for counter

--- src/operators/counter.spec.ts
+++ src/operators/counter.spec.ts
@@ -1,20 +1,19 @@
-import {of} from 'rxjs';
-import {toArray} from 'rxjs/operators';
+import {TestScheduler} from 'rxjs/testing';
 import {counter} from './counter';
 
 describe(counter.name, () => {
-    it('should increment a number for each value', async () => {
-        const v = await of('a', 'b', 'c', 'd', 'e').pipe(
-            counter(),
-            toArray()
-        ).toPromise();
-
-        expect(v).toEqual([
-            [1, 'a'],
-            [2, 'b'],
-            [3, 'c'],
-            [4, 'd'],
-            [5, 'e']
-        ]);
+    it('should increment a number for each value', () => {
+        const scheduler = new TestScheduler((a, e) => expect(a).toEqual(e));
+        scheduler.run(({cold, expectObservable}) => {
+            const o$ = cold('a-b-c-d-e|').pipe(counter());
+            const expect = 'a-b-c-d-e|';
+            expectObservable(o$).toBe(expect, {
+                a: [1, 'a'],
+                b: [2, 'b'],
+                c: [3, 'c'],
+                d: [4, 'd'],
+                e: [5, 'e']
+            });
+        });
     });
 });
