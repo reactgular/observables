2b5c48cf44227979d078cb37ef9e794a4eaadd53
codemile <nick@reactgular.com>
2019-09-21 11:32:52 -0400

    updated coverage for distinctArray

--- src/operators/distinct-array.spec.ts
+++ src/operators/distinct-array.spec.ts
@@ -1,5 +1,5 @@
 import {marbles} from 'rxjs-marbles';
-import {distinctArray} from './distinct-array';
+import {distinctArray, mapToSorted, sortedEqual, unmapSorted} from './distinct-array';
 
 describe('distinctArray', () => {
     it('should emit unique arrays ignoring order of values', marbles(m => {
@@ -35,4 +35,104 @@ describe('distinctArray', () => {
         const expect = '       a-a-c-c-e|';
         m.expect(source).toBeObservable(expect);
     }));
+});
+
+describe('mapToSorted', () => {
+    const varies = [0, undefined, NaN, {}, [], 'FooBar'];
+
+    it('should always return an array', () => {
+        varies.forEach(value => {
+            const m = mapToSorted(value);
+            expect(m instanceof Array).toBe(true);
+            expect(m.length).toBe(2);
+        });
+    });
+
+    it('should always return the original value as first value', () => {
+        varies.forEach(value => {
+            const m = mapToSorted(value);
+            expect(m[0]).toBe(value);
+        });
+    });
+
+    it('should set second value as undefined', () => {
+        [0, undefined, NaN, {}, 'FooBar'].forEach(value => {
+            const m = mapToSorted(value);
+            expect(m[0]).toBe(value);
+            expect(m[1]).toBeUndefined();
+        });
+    });
+
+    it('should sort the array as second value', () => {
+        [
+            [[1, 2, 3], [1, 2, 3]],
+            [[4, 2, 3, 1], [1, 2, 3, 4]],
+            [['a', 'c', 'q', 'r'], ['a', 'c', 'q', 'r']]
+        ].forEach(([first, second]) => {
+            const m = mapToSorted(first);
+            expect(m[0]).toBe(first);
+            expect(m[1]).not.toBe(first);
+            expect(m[1]).toEqual(second);
+        });
+    });
+});
+
+describe('unmapSorted', () => {
+    it('should return first array value', () => {
+        [
+            [1, 2],
+            ['first', 'second'],
+            [NaN, undefined]
+        ].forEach(value => {
+            expect(unmapSorted(value as any)).toBe(value[0]);
+        });
+    });
+});
+
+describe('sortedEqual', () => {
+    it('should return false for non arrays', () => {
+        const notArray = [0, undefined, NaN, {}, 'FooBar'];
+        notArray.forEach(value => {
+            expect(sortedEqual([value, undefined], [value, undefined])).toBe(false);
+            expect(sortedEqual([value, undefined], [[1, 2, 3], [1, 2, 3]])).toBe(false);
+            expect(sortedEqual([[1, 2, 3], [1, 2, 3]], [value, undefined])).toBe(false);
+        });
+    });
+
+    it('should return true for identical array references', () => {
+        const values = [[], [1], ['a'], [undefined]];
+        values.forEach(value => {
+            expect(sortedEqual([value, [...value]], [value, [...value]])).toBe(true);
+        });
+    });
+
+    it('should return false for arrays of different length', () => {
+        const values = [[], [1], ['a'], [undefined]];
+        values.forEach(value => {
+            const v1 = [[...value], [...value]] as any;
+            const v2 = [[...value, 0], [...value, 0]] as any;
+            expect(sortedEqual(v1, v2)).toBe(false);
+        });
+    });
+
+    it('should return false for arrays with different values', () => {
+        const sizes = [1, 3, 5, 10];
+        sizes.forEach(size => {
+            const v1 = Array(size).fill(0).map(() => Math.random());
+            const v2 = v1.map(v => v + 1);
+            expect(sortedEqual(mapToSorted(v1), mapToSorted(v2))).toBe(false);
+        });
+    });
+
+    it('should return true for arrays with same values, but in different orders', () => {
+        const sizes = [3, 5, 10, 15, 20];
+        sizes.forEach(size => {
+            let v1, v2;
+            while (JSON.stringify(v1) === JSON.stringify(v2)) {
+                v1 = Array(size).fill(0).map(() => Math.random());
+                v2 = [...v1].sort(() => Math.random());
+            }
+            expect(sortedEqual(mapToSorted(v1), mapToSorted(v2))).toBe(true);
+        });
+    });
 });
--- src/operators/distinct-array.ts
+++ src/operators/distinct-array.ts
@@ -4,35 +4,47 @@ import {distinctUntilChanged, map} from 'rxjs/operators';
 /**
  * Only emits when an array contains different values than the last and ignores the order of those values.
  */
-export function distinctArray<T>(): MonoTypeOperatorFunction<T[]> {
-    return (source: Observable<T[]>): Observable<T[]> => {
+export function distinctArray<T>(): MonoTypeOperatorFunction<T> {
+    return (source: Observable<T>): Observable<T> => {
         return source.pipe(
-            map(arr => ([arr, Array.isArray(arr) ? [...arr].sort() : arr])),
-            distinctUntilChanged((prev, next) => sortedEqual(prev[0], prev[1], next[0], next[1])),
-            map(([arr]) => arr)
+            map(mapToSorted),
+            distinctUntilChanged((prev, next) => sortedEqual(prev, next)),
+            map(unmapSorted)
         );
     };
 }
 
 /**
- * Sorts the two arrays before comparing if they contain the same values.
+ * Maps an array to a tuple of the original and sorted.
  */
-function sortedEqual<T>(a: T[], sortA: T[], b: T[], sortB: T[]): boolean {
-    if (!Array.isArray(a) || !Array.isArray(b)) {
+export const mapToSorted = <T>(arr: T): [T, T] => [arr, Array.isArray(arr) ? [...arr].sort() as any as T : undefined];
+
+/**
+ * Unmaps the tuple of the original and sorted back to just the original.
+ */
+export const unmapSorted = <T>([arr]: [T, T]): T => arr as T;
+
+/**
+ * Sorts the two arrays before comparing if they contain the same values. This function is used internally, and expects
+ * the arguments to contain both the original array value, and the sorted variant of that value. The code doesn't assert
+ * this for performance reasons.
+ */
+export function sortedEqual<T>(a: [T, T], b: [T, T]): boolean {
+    if (!Array.isArray(a[0]) || !Array.isArray(b[0])) {
         return false;
     }
 
-    if (a === b) {
+    if (a[0] === b[0]) {
         return true;
     }
 
-    const length = a.length;
-    if (length !== b.length) {
+    const length = a[0].length;
+    if (length !== b[0].length) {
         return false;
     }
 
     for (let i = length; i-- !== 0;) {
-        if (sortA[i] !== sortB[i]) {
+        if (a[1][i] !== b[1][i]) {
             return false;
         }
     }
