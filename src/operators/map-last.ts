96066d94bc2470af780e66deed96dc0a1144e982
codemile <nick@reactgular.com>
2019-09-21 13:37:15 -0400

    updated

--- src/operators/map-last.ts
+++ src/operators/map-last.ts
@@ -3,8 +3,9 @@ import {endWith, map, pairwise} from 'rxjs/operators';
 
 /**
  * Applies a given `project` function to the last value emitted by the source Observables, and emits the resulting value. Only the last
- * value is projected and previous values are emitted without projection. This operator uses [pairwise()](https://rxjs.dev/api/operators/pairwise)
- * internally and emits each value only when a next value is emitted or the source observable completes.
+ * value is projected and previous values are emitted without projection.
+ * This operator uses [pairwise()](https://rxjs.dev/api/operators/pairwise) internally and emits each value only when a next value is
+ * emitted or the source observable completes.
  */
 export function mapLast<T, R>(project: (value: T) => R): OperatorFunction<T, T | R> {
     return (source: Observable<T>): Observable<T | R> => {
